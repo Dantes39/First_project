277109e6cf9fc0006a14bbc04413b1b35e6d9af1
Dantes39 <56196765+Dantes39@users.noreply.github.com>
2019-11-27 18:11:23 +0200

    Add files via upload

---
+++ electric_pole.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def magnit_pole(q,r):
+    k = 9*10**9
+    E = k*q/r**2
+    return E
+
+def ell_circl_plotter():
+    fig, ax = plt.subplots()
+    a = 4
+    x = np.arange(-a,a,0.01)
+    y = np.arange(-a,a,0.01)
+    X,Y = np.meshgrid(x,y)
+    fxy = X**2 + Y**2
+    elpole = ax.contour(X,Y,fxy, levels = [1,2,5,7,9])
+    manual_locations = [(1,4),(1,8),(3,6)]
+    ax.clabel(elpole, inline=1, fontsize=10, manual=manual_locations)
+    E = magnit_pole(10,1)
+    plt.plot(1,1, color='y',label='{}'.format(E))
+    plt.legend()
+    
+    plt.axis('equal')
+    plt.show()
+    plt.savefig('gfhj')
+    
+magnit_pole(3)
+ell_circl_plotter()
+    
